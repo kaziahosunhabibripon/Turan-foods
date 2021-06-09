9271ff6cb0547a01725eadc991f69ff0de09579a
Ripon <habibkazi92@gmail.com>
2021-06-09 18:25:38 +0600

    minuse plus button work done

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import {createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from "@react-navigation/native";
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Restaurant, OrderDelivery, Home} from './screens';
+import Tabs from './navigation/Tabs';
+const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = ()=>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName ={"Home"}
+
+      >
+        <Stack.Screen name="Home" component={Tabs}/>
+        <Stack.Screen name="Restaurant" component={Restaurant}/>
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
